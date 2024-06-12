a6c361e8c15f58112d691edde33974263a6f28d8
Joseph Pinzone <jpinnoz@yahoo.com>
2024-06-12 19:01:12 +1000

    Split First and Last Name filtering in userdetails.php

--- userdetails.php
+++ userdetails.php
@@ -11,10 +11,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" AND $_POST['submitCode']=="User_Details
 		$firstName = $_POST['ffirstname'];
 		$lastName = $_POST['flastname'];
 		$userName = $_SESSION['user_name'];
-		
-		if (!preg_match("/^[A-Za-z\-\'\ ]{1,100}$/", $firstName) OR !preg_match("/^[A-Za-z\-\'\ ]{1,100}$/", $lastName)) {
-			$errormessage .= "<div class='error'>You may only use letters, hyphens or apostraphes! Minumum length is 1, maximum length is 100!</div><p>\r\n";
-		} else {
+		$error_style = "style='background-color:pink'";
+		$error_count = 0;
+
+		if (!preg_match("/^[A-Za-z\-\'\ ]{1,100}$/", $firstName)) {
+			$err_count++;
+			$ffirstname_style=$error_style;
+			$message .= "<div class='error'>First Name input ERROR: You may only use letters, hyphens, spaces or apostraphes! Minumum length is 1, maximum length is 100!</div><p>\r\n";
+		}
+		if (!preg_match("/^[A-Za-z\-\'\ ]{1,100}$/", $lastName)) {
+			$err_count++;
+			$flastname_style=$error_style;
+			$message .= "<div class='error'>Last Name input ERROR: You may only use letters, hyphens, spaces or apostraphes! Minumum length is 1, maximum length is 100!</div><p>\r\n";
+		}
+		if ($err_count==0) {
 			$query2 = "UPDATE `Users` SET `First_Name`='$firstName',`Last_Name`='$lastName' WHERE `User_Name`='$userName'";
 			mysqli_query($cxn,$query2);
 			
@@ -40,13 +50,13 @@ if (isset($_SESSION['user_name'])) {
 	$firstName = $row1['First_Name'];
 	$lastName = $row1['Last_Name'];
 
-	echo "$errormessage\r\n
+	echo "$message\r\n
 	<h3>User Details</h3>\r\n
 	<div>\r\n
 	<form action='".htmlentities($_SERVER['PHP_SELF'])."' method='post'>\r\n
 	<table>\r\n
-	<tr><td>First Name: </td><td><input type='text' name='ffirstname' value='$firstName'></td></tr>\r\n
-	<tr><td>Last Name: </td><td><input type='text' name='flastname' value='$lastName'></td></tr>\r\n
+	<tr><td>First Name: </td><td><input $ffirstname_style type='text' name='ffirstname' value='$firstName'></td></tr>\r\n
+	<tr><td>Last Name: </td><td><input $flastname_style type='text' name='flastname' value='$lastName'></td></tr>\r\n
 	</table>\r\n
 	<input type='hidden' id='submitCode' name='submitCode' value='User_Details'>\r\n
 	<input type='submit' name='Button' value='Update' />\r\n
